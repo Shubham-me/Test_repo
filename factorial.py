037709389be057b63cae7327bcf4e23ecb64eaa4
Shubham-me <shubhv512@gmail.com>
2024-05-28 10:34:23 +0530

    Changed factorial to iterative

--- factorial.py
+++ factorial.py
@@ -1,6 +1,7 @@
 def factorial(n):
-    if(n == 0):
-        return 1
-    return n * factorial(n - 1)
+    res = 1
+    for i in range(2,n+1):
+        res *= i
+    return res
 n = 10
 print(factorial(n))
