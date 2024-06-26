f5073faaac8873981fa4ed181dbe9905f3bf6ff0
Shubham <87982899+Shubham-me@users.noreply.github.com>
2024-06-26 17:34:46 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,7 +1,8 @@
+from multiplication import multiply
 def factorial(n):
     res = 1
     for i in range(2,n+1):
-        res *= i
+        res = multiply(res,i)
     return res
 n = 20
 print(factorial(n))
